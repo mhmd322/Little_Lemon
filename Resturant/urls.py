62b2278cdbf45f29a77e74b831f3b87de8880fa4
Mohamad Qasem <mhmd322kasem@gmail.com>
2025-07-05 07:56:03 +0400

    create serialaizer

--- Resturant/urls.py
+++ Resturant/urls.py
@@ -1,8 +1,14 @@
-from django.contrib import admin 
-from django.urls import path 
-from . import views
-  
-urlpatterns = [ 
-    
-    path('', views.index, name='home'),
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MenuViewSet, BookingViewSet, index  # استيراد مباشر لكل شيء
+
+# إعداد الراوتر
+router = DefaultRouter()
+router.register(r'menu', MenuViewSet)
+router.register(r'booking', BookingViewSet)
+
+# دمج الروابط
+urlpatterns = [
+    path('', index, name='home'),          # الصفحة الرئيسية
+    path('api/', include(router.urls)),    # روابط API تلقائية
 ]
