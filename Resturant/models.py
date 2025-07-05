7b2b96a7af2f976e32db3d5cbf3f40219ad257aa
Mohamad Qasem <mhmd322kasem@gmail.com>
2025-07-05 06:48:39 +0400

    create models and data

--- Resturant/models.py
+++ Resturant/models.py
@@ -1,3 +1,23 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)   # معرف تلقائي كـ مفتاح أساسي
+    name = models.CharField(max_length=100)
+    no_of_guests = models.PositiveIntegerField()
+    booking_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} - {self.booking_date}"
+
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)   # معرف تلقائي كـ مفتاح أساسي
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
